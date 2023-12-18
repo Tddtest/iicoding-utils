a95f4f1828adefa53913b0963251e75906096126
iiCoding <297854770@qq.com>
2023-12-18 19:46:09 +0800

    feat: 新增copy

--- dist/cjs/helper/index.d.ts
+++ dist/cjs/helper/index.d.ts
@@ -1,7 +1,8 @@
+export * from './copy';
 export declare const sleep: (time: number) => Promise<unknown>;
 export declare const phoneDesensitization: (phone: string) => string;
 export declare const credentialDesensitization: (credential: string) => string;
-export declare const IdCardGender: <P = any>(idCard: string, placeholder?: P) => P | "0" | "1";
+export declare const IdCardGender: <P = any>(idCard: string, placeholder?: P) => "0" | P | "1";
 export declare const toFormData: (target: Record<string, any>) => FormData;
 export declare const getRandomColor: (color?: string) => string;
 export declare const color2hexadecimal: (hex: string, onlyValue?: boolean) => string | (string | number)[];
